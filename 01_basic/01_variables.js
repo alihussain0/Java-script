240921d71f8bf36e4a98e9d7b4a3085434ced035
alihussain0 <57325235+alihussain0@users.noreply.github.com>
2025-07-29 08:34:53 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+ const accoundId = 12445
+ let accountEmail ="alirahber0@gmail.com"
+ var accountPassword = "12345"
+ accountCity = "Jaipur"
+
+//  accoundId = 2
+    
+    accountEmail = "rahber@gmail.com"
+    accountPassword = "2345"
+    accountCity = "delhi"
+
+ console.log(accoundId);
+
+ console.table([accoundId, accountEmail, accountPassword, accountCity])
+ 
